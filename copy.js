48314cb8c91619a9aa89af8f63b3e9db21cdbb99
bipul <itsmebipul00@gmail.com>
2023-02-22 22:29:58 +0530

    Add

--- copy.js
+++ copy.js
@@ -1,24 +1,94 @@
-;(() => {
+function sleepThree() {
+	return new Promise(resolve => {
+		const timerThree = setTimeout(() => {
+			const dontKnow = document.querySelector(
+				'[aria-label="We don\'t know each other"]'
+			)
+			const connect = document.querySelector('[aria-label="Connect"]')
+			console.log(dontKnow, connect)
+			if (!connect && !dontKnow) {
+				clearTimeout(timerThree)
+			} else if (!dontKnow) {
+				connect.click()
+			} else {
+				dontKnow.click()
+				connect.click()
+			}
+			resolve()
+		}, 1500)
+	})
+}
+
+function sleepFour() {
+	return new Promise(resolve => {
+		const timerFour = setTimeout(() => {
+			const sendbtn = document.querySelector(
+				'[aria-label="Send now"]'
+			)
+			if (!!sendbtn) {
+				sendbtn.click()
+			} else clearTimeout(timerFour)
+			resolve()
+		}, 1500)
+	})
+}
+
+function sleepTwo() {
+	return new Promise(resolve => {
+		const timerTwo = setTimeout(() => {
+			const connect = document.querySelector('[aria-label="Connect"]')
+			if (!connect) {
+				clearTimeout(timerTwo)
+			} else {
+				connect.click()
+			}
+			resolve()
+		}, 1500)
+	})
+}
+
+// function sleepOne(i, btns) {
+// 	return new Promise(resolve =>
+// 		setTimeout(
+// 			async i => {
+// 				btns[i].click()
+// 				await sleepThree()
+// 				await sleepTwo()
+// 				await sleepFour()
+// 				resolve()
+// 			},
+// 			2000,
+// 			i
+// 		)
+// 	)
+// }
+
+;(async () => {
 	const connectBtns = document.querySelectorAll('button')
 	const btns = [...connectBtns].filter(
 		btn => btn.innerText === 'Connect'
 	)
-	// console.log(connectBtns)
 	let i = 0
+	let timer
 	while (i < btns.length) {
-		setTimeout(() => {
-			btns[i].click()
-			setTimeout(() => {
-				const sendbtn = document.querySelector(
-					'[aria-label="Send now"]'
-				)
-				if (!!sendbtn) {
-					sendbtn.click()
-				} else return
-			}, 1500)
-			i++
-		}, 3000)
-		console.log('btns')
-		console.log(i)
+		await new Promise(resolve => {
+			timer = setTimeout(
+				async i => {
+					btns[i].click()
+					await sleepThree()
+					await sleepTwo()
+					await sleepFour()
+					resolve()
+				},
+				3000,
+				i
+			)
+		})
+		i++
+		await chrome.runtime.sendMessage({
+			total: btns.length,
+			current: i,
+			timer: timer,
+		})
 	}
 })()
--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const conectBtn = document.querySelector('.connect-btn')
+const invitations = document.querySelector('.invitations')
 
 const handleConnect = async () => {
-	const tab = await getCurrentTab()
-	console.log(tab)
-	const { id, url } = tab
-	chrome.scripting.executeScript({
-		target: { tabId: id, allFrames: true },
-		files: ['copy.js'],
-	})
+	if (conectBtn.innerText === 'Connect') {
+		const tab = await getCurrentTab()
+		console.log(tab)
+		const { id, url } = tab
+		chrome.scripting.executeScript({
+			target: { tabId: id, allFrames: true },
+			files: ['copy.js'],
+		})
+		conectBtn.innerText = 'Stop'
+	} else {
+		conectBtn.innerText = 'Stop'
+	}
 }
 
 const getCurrentTab = async () => {
@@ -15,5 +21,13 @@ const getCurrentTab = async () => {
 	let [tab] = await chrome.tabs.query(queryOptions)
 	return tab
 }
+
+chrome.runtime.onMessage.addListener(function (request) {
+	console.log(request)
+	invitations.innerText = `${request.current} of ${request.total} requests sent`
+	if (conectBtn.innerText === 'Stop') {
+		clearTimeout(request.timer)
+	}
+})
 
 conectBtn.addEventListener('click', handleConnect)
